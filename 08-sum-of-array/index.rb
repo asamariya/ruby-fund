dc3a78574a0c50456b2210d4696c2f68aee8a24d
Abhishek Samariya <asamariya@gmail.com>
2020-02-19 13:57:48 +1100

    All tests pass

--- 08-sum-of-array/index.rb
+++ 08-sum-of-array/index.rb
@@ -1,1 +1,5 @@
 # Define a method called sum_of_array that takes an array of numbers, and returns the total of the addition of all the numbers in the array.
+
+def sum_of_array(num_arr)
+  num_arr.sum
+end
