8a3319fc7a301aaf57b0c38ef603ac5c7ab62b8c
Abhishek Samariya <asamariya@gmail.com>
2020-02-19 15:22:34 +1100

    Solved - all tests pass

--- 11-the-hipster-problem/index.rb
+++ 11-the-hipster-problem/index.rb
@@ -5,3 +5,16 @@
 # Write a second method called calculate_volume that takes two number arguments, one representing the area of the top of the table, and the other representing the height (from the ground). Return the volume.
 
 # Write a third method called calculate_table_volume that performs no calculations, but takes three number arguments and uses the previous two methods to return the volume of the table.
+
+def calculate_area(num1, num2)
+  (num1 * num2) / 2
+end
+
+def calculate_volume(area, height)
+  area * height
+end
+
+def calculate_table_volume(num1, num2, num3)
+  area = calculate_area(num1, num2)
+  calculate_volume(area, num3)
+end
